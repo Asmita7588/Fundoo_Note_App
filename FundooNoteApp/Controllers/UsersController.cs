5ec232081905016aecd47a0e8a3fa8214c6af953
Asmita Girhepunje <girhepunjeasmita25@gmail.com>
2025-03-25 14:58:38 +0530

    FundooNoteApp Api

---
+++ FundooNoteApp/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using CommonLayer.Models;
+using MangerLayer.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RepositoryLayer.Entity;
+
+namespace FundooNoteApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserManager userManager;
+
+        public UsersController(IUserManager userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        //httplocal/api/Users/Reg
+        [HttpPost]
+        [Route("Reg")]
+
+        public IActionResult Register(RegisterModel model) {
+
+            var result = userManager.Register(model);
+            if (result != null) {
+                return Ok(new ResponseModel<UserEntity> { Success = true, Message = "Register successfully", Data = result });
+
+            }
+            return BadRequest(new ResponseModel<UserEntity> { Success = true, Message = "Register successfully", Data = result });
+        }
+    }
+}
---
+++ MangerLayer/Services/UserManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommonLayer.Models;
+using MangerLayer.Interfaces;
+using RepositoryLayer.Entity;
+using RepositoryLayer.Interfaces;
+using RepositoryLayer.Services;
+
+namespace MangerLayer.Services
+{
+    public class UserManager : IUserManager
+    {
+        private readonly IUserRepo userRepo;
+
+        public UserManager(IUserRepo userRepo)
+        {
+            this.userRepo = userRepo;
+        }
+
+        public UserEntity Register(RegisterModel model) { 
+
+            return userRepo.Register(model);
+        }
+    }
+}
